1ffa9f73905aa285177b33a58003cb85e77629c8
Enoc Velasquez <fungicoder@gmail.com>
2022-07-04 12:46:27 -0600

    initial template

---
+++ src/main/java/com/fordevs/xml2json/Xml2jsonApplication.java
@@ -0,0 +1,13 @@
+package com.fordevs.xml2json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xml2jsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Xml2jsonApplication.class, args);
+	}
+
+}
